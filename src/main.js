83efaa14d5f6cfea639afa5b4742b853fc3aaaf9
nbhuangzl <your_email@youremail.com>
2021-06-24 17:29:30 +0800

    引入vant组件

--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ import router from './router/router'
 import store from './store'
 // 加载全局样式
 import './styles/index.less'
+// npm i vant
+// 加载 Vant 核心组件
+import Vant from 'vant'
+// 加载 Vant全局样式
+import 'vant/lib/index.css'
+// 注册使用Vant组件库
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
